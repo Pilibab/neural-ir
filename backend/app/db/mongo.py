cbe98aca6ce5db4b93b8ee8d486bca64b3f273de
Prometheus <toshiyasato219@gmail.com>
2026-01-10 22:27:17 +0900

    refactor(db): removed methods on mongo.py - reason: to allow collection method for different collection

--- backend/app/db/mongo.py
+++ backend/app/db/mongo.py
@@ -1,4 +1,4 @@
-# creates MongoClient and db
+# mongo.py: creates MongoClient and db
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
@@ -12,18 +12,3 @@ db = client.manhwa_db
 
 manhwa_vector_collection = db.manhwa_vectors
 manhwa_data_collection = db.manhwa_data
-
-def find_by_source(source, source_id):
-    return manhwa_data_collection.find_one({
-        "source": source,
-        "source_id": source_id
-    })
-
-def insert(doc):
-    manhwa_data_collection.insert_one(doc)
-
-def update(_id, doc):
-    manhwa_data_collection.update_one(
-        {"_id": _id},
-        {"$set": doc}
-    )

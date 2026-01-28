f53d525f4f3c3575251c38a0a516c4479dced18b
Prometheus <toshiyasato219@gmail.com>
2026-01-28 21:15:22 +0900

    refactor: moved the types outside the search component's scope to src/domain/manhwa,

---
+++ frontend/src/domain/manhwa/VectorSearchMeta.ts
@@ -0,0 +1,8 @@
+
+export interface VectorSearchMeta {
+    title: string;
+    source: string;
+    source_id: string;
+    embedding_source: string;
+    final_score: number;
+}
--- frontend/src/feature/search/types.ts
+++ frontend/src/feature/search/types.ts
@@ -1,30 +1,7 @@
-// ! Move this? since files from higher folder also uses this
+import type { VectorSearchMeta } from "../../domain/manhwa/VectorSearchMeta";
 
-export interface ManhwaResult {
-    rank: number;
-    title: string;
-    synopsis: string;
-    cover_image_url: string;
-    rating: number;
-    chapters: string | number;
-    published_date: string;
-    tags: string;
-    link: string;
+
+export interface ResultsPanelProps {
+    currIdx: number,
+    resultsVectorSearch: VectorSearchMeta[];
 }
-
-export interface VectorSearchMeta {
-    title: string;
-    source: string;
-    source_id: string;
-    embedding_source: string;
-    final_score: number;
-}
-
-// ==============================
-// type of each key-value
-// ==============================
-// Key: title                | Types: [str]
-// Key: source               | Types: [str]
-// Key: source_id            | Types: [int]
-// Key: embedding_source     | Types: [str]
-// Key: final_score          | Types: [float]
